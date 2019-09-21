060f17c042567e74b548ecd0f05261f4bdbaf0eb
jmarkaba <jmarkaba@asu.edu>
2019-09-21 09:56:03 -0700

    updated string processor

--- string_processing/processor.py
+++ string_processing/processor.py
@@ -1,13 +1,27 @@
 import wikipedia as wiki
-
-print(wiki.summary("Barack Obama"))
-
+import nltk
+from gensim.summarization import summarizer
 
 def _wikisearch(query):
     results = wiki.search(query, results=4)
     return results
 
-def get_page_from_list(query_list, option_selected):
+def _get_page_from_list(query_list, option_selected):
     selected = query_list[option_selected]
-    return wiki.summary()
+    summary = wiki.summary(selected)
+    print("1.")
+    print(summary, end="\n\n\n")
+    return summary
     
+def _summarize_data(full_string):
+    condensed = summarizer.summarize(full_string)
+    print("2.")
+    print(condensed)
+    return condensed
+
+def test(string):
+    query_list = _wikisearch(string)
+    page_sum = _get_page_from_list(query_list, 1)
+    return _summarize_data(page_sum)
+
+test("Stockfish")
