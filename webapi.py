888262f5d3b8040901ba251947b6134c8861369c
Adheesh Wadkar <apwadkar@asu.edu>
2019-09-21 14:37:05 -0700

    Fixed messaging?

--- webapi.py
+++ webapi.py
@@ -52,7 +52,7 @@ def sms_recv_msg():
                 texts.append(message[counter: counter + TEXTSIZE])
                 counter = counter + TEXTSIZE
             texts.append(message[counter:len(message)])
-            resp.messages(texts)
+            resp.message(texts)
         else:
             resp.message("Sorry, I couldn't understand")
     else:
